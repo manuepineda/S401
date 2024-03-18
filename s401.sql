fd01a65d0e30f3107dffa1eb9284183358608ab5
manuepineda <107059847+manuepineda@users.noreply.github.com>
2024-03-18 13:50:35 +0100

    Add files via upload

--- s401.sql
+++ s401.sql
@@ -2,6 +2,8 @@
 
 #Ej 1
 
+CREATE DATABASE s401;
+
 #creamos la tablas en la que introduciremos los datos
 
 CREATE TABLE IF NOT EXISTS companies (
@@ -66,9 +68,9 @@ user_id VARCHAR(50),
 lat VARCHAR(50),
 longitude VARCHAR(50),
 FOREIGN KEY(card_id) REFERENCES credit_cards (id),
-FOREIGN KEY (business_id) REFERENCES companies(company_id),
-FOREIGN KEY (product_ids) REFERENCES products(id),  
-FOREIGN KEY (user_id) REFERENCES users(id)   
+FOREIGN KEY (business_id) REFERENCES companies (company_id),
+FOREIGN KEY (product_ids) REFERENCES products (id),  
+FOREIGN KEY (user_id) REFERENCES users (id)   
 );
 
 SELECT * FROM companies;
@@ -76,3 +78,63 @@ SELECT * FROM credit_cards;
 SELECT * FROM products;
 SELECT * FROM transactions;
 SELECT * FROM users;
+
+#EJ1
+
+SELECT user_id, transaction_count
+FROM ( 
+	select user_id, COUNT(*) AS transaction_count
+    FROM transactions
+    GROUP BY user_id
+    ) AS user_transactions
+    WHERE transaction_count > 5;
+
+#EJ2 
+
+SELECT transactions.business_id
+FROM transactions 
+JOIN companies
+ON transactions.business_id = companies.company_id
+WHERE transactions.business_id = "b-22422"
+group by transactions.business_id;
+
+SELECT Iban, Avg(total_amount)
+from (
+	SELECT credit_cards.Iban, SUM(transactions.amount) AS total_amount
+    FROM credit_cards
+    JOIN transactions ON credit_cards.id = transactions.card_id
+    JOIN companies ON transactions.business_id = companies.company_id
+    WHERE transactions.declined = '0' AND companies.company_name = 'Donec Ltd'
+    GROUP BY credit_cards.Iban ) AS transaction_totals
+group by Iban;
+
+
+
+SELECT credit_cards.Iban, SUM(transactions.amount) AS total_amount
+    FROM credit_cards
+    JOIN transactions ON credit_cards.id = transactions.card_id
+    JOIN companies ON transactions.business_id = companies.company_id
+    WHERE Companies.company_name = 'Donec Ltd'
+    GROUP BY credit_cards.Iban;
+    
+    SELECT * FROM transactions
+    where companies.company_name = "Donec Ltd";
+
+
+SELECT business_id, COUNT(card_id) AS card_count
+FROM transactions
+JOIN companies ON transactions.business_id = companies.company_id
+where company_name LIKE 'Donec Ltd%'
+GROUP BY business_id;
+
+
+#NIVEL 2
+
+
+
+#EJ 1
+
+
+
+
+
